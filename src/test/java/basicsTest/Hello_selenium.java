24d1546a644d69bb10ceb9257cfa80775f9a0702
Ramani <ramanipriyamech@gmail.com>
2026-03-23 18:13:27 +0530

    This is demo project initialization version 1

---
+++ src/test/java/basicsTest/Hello_selenium.java
@@ -0,0 +1,17 @@
+package basicsTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Hello_selenium {
+    @Test
+    public void openBrowser(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+        driver.get("https://www.amazon.in/");
+    }
+
+
+}
